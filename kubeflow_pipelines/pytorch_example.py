917ed79e7b7c36ac9e1a032d4e44290552a02a25
Isaac Godfried <igodfried@isaac26.com>
2019-06-20 16:20:23 -0600

    add import statements to code

--- kubeflow_pipelines/pytorch_example.py
+++ kubeflow_pipelines/pytorch_example.py
@@ -1,3 +1,7 @@
+#https://www.kubeflow.org/docs/pipelines/sdk/dsl-overview/
+import kfp
+from kfp import dsl
+
 @kfp.dsl.pipeline(
   name='PyTorch example',
   description='A simple PyTorch pipeline in Kubeflow'
