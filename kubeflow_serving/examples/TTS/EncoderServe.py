d51c1cfa36da21a1df3111db34896861fa97a1ff
Isaac Godfried <igodfried@isaac26.com>
2019-07-11 16:41:09 -0400

    update serve

--- kubeflow_serving/examples/TTS/EncoderServe.py
+++ kubeflow_serving/examples/TTS/EncoderServe.py
@@ -3,13 +3,15 @@ from encoder import inference as encoder
 class EncoderServe(object):
     def __init__(path_weight=""):
         encoder.load_model(path_weight)
-    def predict(X):
-        preprocessed_wav = encoder.preprocess_wav(X)
+    def predict(x, feature_names):
+        #preprocessed_wav = encoder.preprocess_wav(x)
         # - If the wav is already loaded:
-        original_wav, sampling_rate = librosa.load(in_fpath)
-        preprocessed_wav = encoder.preprocess_wav(original_wav, sampling_rate)
-        print("Loaded file succesfully")
+        #original_wav, sampling_rate = librosa.load(x)
+        #preprocessed_wav = encoder.preprocess_wav(original_wav, sampling_rate)
+        #print("Loaded file succesfully")
         # Then we derive the embedding. There are many functions and parameters that the 
         # speaker encoder interfaces. These are mostly for in-depth research. You will typically
         # only use this function (with its default parameters):
-        embed = encoder.embed_utterance(preprocessed_wav)
+        embed = encoder.embed_utterance(x)
+
+EncoderServe()
