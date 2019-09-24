f1e26e7197b898e15a560fa8406000f97a826fac
isaacmg <igodfried@isaac26.com>
2019-09-24 11:12:59 -0400

    add model code etc
    Add code for all the models

---
+++ kubeflow_serving/examples/deblur_gan/auxillary_functions.py
@@ -0,0 +1,9 @@
+def get_normalize():
+    normalize = albu.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+    normalize = albu.Compose([normalize], additional_targets={'target': 'image'})
+
+    def process(a, b):
+        r = normalize(image=a, target=b)
+        return r['image'], r['target']
+
+    return process
